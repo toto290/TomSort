93bb5c72d25f57c0362fc8baec58a2b7008d0951
toto290 <toto290@users.noreply.github.com>
2019-11-16 21:39:23 +0000

    reworked GUI for cleaner and slimmer design

--- Main.py
+++ Main.py
@@ -3,128 +3,109 @@ import os
 from pathlib import Path
 
 
-class MainApplication(tk.Frame):
-    def __init__(self, parent, title):
-        tk.Frame.__init__(self, parent)
-        self.root = parent
+class MainApplication(tk.Tk):
+    def __init__(self, title):
+        tk.Tk.__init__(self)
+
+        # initial parameters
         self.mode = tk.IntVar()
         self.mode.set(0)    # start:0 sort:1 twin:2
-        #self.root.state('zoomed')
-        self.root.geometry('1000x500')
-        self.root.title(title)
+        self.width = 500
+        self.height = 500
+        self.geometry('{}x{}'.format(str(self.width), str(self.height)))
+        self.title(title)
+        self.update_idletasks()
 
+        # GUI creation
         self.menu = Menu(self)
-        self.status = Status(self.root)
+        self.status = Status(self)
+        self.frame_start = FrameModeStart(self, width=self.width, height=self.height)
+        self.frame_sort = FrameModeSort(self, width=self.width, height=self.height)
+        self.frame_twin = FrameModeTwin(self, width=self.width, height=self.height)
+        self.mode_dict = {0: ('Start', self.frame_start), 1: ('Sort', self.frame_sort), 2: ('Twin', self.frame_twin)}
 
-        self.root.update()
-        root_width = self.root.winfo_width()
-        root_height = self.root.winfo_height()
-        print('Frame root is ' + str(root_width) + ' wide and ' + str(root_height) + ' high')
-        self.frame_start = FrameModeStart(self.root, width=root_width, height=root_height)
-        self.frame_sort = FrameModeSort(self.root, width=root_width, height=root_height)
-        self.frame_twin = FrameModeTwin(self.root, width=root_width, height=root_height)
-        self.currentframe = self.frame_start
-        self.updateinterface()
+        # GUI initialization
+        self.currentframe = self.mode_dict[0][1]
+        self.status.statustext.set(self.mode_dict[0][0] + '-Mode')
+        self.currentframe.pack()
+        self.update_idletasks()
 
-    def updateinterface(self):
-        print('updating interface - mode: ' + str(self.mode.get()))
+        # Binds
+        self.bind("<Configure>", lambda e: self.resize())
+
+    def switch_mode(self, num):
+        print('Switch mode: ' + self.mode_dict[num][0] + '(' + str(num) + ')')
+        self.mode.set(num)
+        self.status.statustext.set(self.mode_dict[num][0] + '-Mode')
         self.currentframe.pack_forget()
-        if self.mode.get() == 0:
-            self.frame_start.packit()
-            self.currentframe = self.frame_start
-        elif self.mode.get() == 1:
-            self.frame_sort.packit()
-            self.currentframe = self.frame_sort
-        elif self.mode.get() == 2:
-            self.frame_twin.packit()
-            self.currentframe = self.frame_twin
+        self.mode_dict[num][1].pack(side="bottom", fill="both", expand=True)
+        self.update_idletasks()
+        self.currentframe = self.mode_dict[num][1]
+
+    def resize(self):
+        self.width = self.winfo_width()
+        self.height = self.winfo_height()
+        print('Resize: ' + str(self.width) + 'x' + str(self.height))
+        self.currentframe.config(width=self.width, height=self.height)
+        self.update_idletasks()
 
 
 class FrameModeStart(tk.Frame):
-    def __init__(self, parent, width, height):
-        tk.Frame.__init__(self, parent, width=width, height=height)
-        self.root = parent
-
-    def packit(self):
-        self.pack(side="bottom", fill="both", expand=True)
+    def __init__(self, root, **kwargs):
+        print('init: Start-Mode Frame')
+        tk.Frame.__init__(self, root, **kwargs)
+        self.root = root
 
 
 class FrameModeSort(tk.Frame):
-    def __init__(self, parent, width, height):
-        tk.Frame.__init__(self, parent, width=width, height=height)
-        self.root = parent
-        self.width = self.root.winfo_width()
-        self.height = self.root.winfo_height()
-        print('Frame ModeSorting is ' + str(self.width) + ' wide and ' + str(self.height) + ' high')
-        self.left_frame = tk.Frame(self, bg='blue', height=int(1 * self.height), width=int(0.7 * self.width))
-        self.right_frame = tk.Frame(self, bg='red', height=int(1 * self.height), width=int(0.3 * self.width))
+    def __init__(self, root, **kwargs):
+        print('init: Sort-Mode Frame')
+        tk.Frame.__init__(self, root, **kwargs)
+        self.root = root
+        self.left_frame = tk.Frame(self, bg='blue', height=int(1 * self.root.height), width=int(0.7 * self.root.width))
+        self.right_frame = tk.Frame(self, bg='red', height=int(1 * self.root.height), width=int(0.3 * self.root.width))
         self.left_frame.pack(side='left', expand='1')
         self.right_frame.pack(side='right', expand='1')
-
-    def packit(self):
-        print('Packed: SortingMode')
-        self.pack(side="bottom", fill="both", expand=True)
 
 
 class FrameModeTwin(tk.Frame):
     def __init__(self, parent, width, height):
+        print('init: Twin-Mode Frame')
         tk.Frame.__init__(self, parent, width=width, height=height)
         self.root = parent
         left_frame = tk.Frame(self, bg='green', height=600, width=600)
         right_frame = tk.Frame(self, bg='yellow', height=600, width=600)
 
-    def packit(self):
-        self.pack(side="bottom", fill="both", expand=True)
-
-
 
 class Menu(tk.Frame):
-    def __init__(self, app):
-        tk.Frame.__init__(self, app.root)
-        self.app = app
-        self.menu = tk.Menu(app.root, tearoff=False)
-        app.root.config(menu=self.menu)
+    def __init__(self, root):
+        tk.Frame.__init__(self, root)
+        self.root = root
+        self.menu = tk.Menu(self.root, tearoff=False)
+        self.root.config(menu=self.menu)
 
         self.submenu_start = tk.Menu(self.menu, tearoff=False)
         self.menu.add_cascade(label='Start', menu=self.submenu_start)
 
         self.submenu_modes = tk.Menu(self.menu, tearoff=False)
         self.menu.add_cascade(label='Modes', menu=self.submenu_modes)
-        self.submenu_modes.add_command(label='Sort Photos', command=self.activate_mode_sort)
-        self.submenu_modes.add_command(label='Find Twins', command=self.activate_mode_twin)
-
-    def activate_mode_sort(self):
-        self.app.mode.set(1)
-        self.app.status.statustext.set('Sorting Mode')
-        print('Mode changed to: Sorting')
-        self.app.updateinterface()
-        
-    def activate_mode_twin(self):
-        self.app.mode.set(2)
-        self.app.status.statustext.set('Twin Finding Mode')
-        print('Mode changed to: TwinFinding')
-        self.app.updateinterface()
+        self.submenu_modes.add_command(label='Sort Photos', command=lambda: self.root.switch_mode(1))
+        self.submenu_modes.add_command(label='Find Twins', command=lambda: self.root.switch_mode(2))
 
 
 class Status(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
-        self.parent = parent
+    def __init__(self, root):
+        tk.Frame.__init__(self, root)
+        self.root = root
         self.statustext = tk.StringVar()
         self.statustext.set('idle')
-        self.status = tk.Label(self.parent, textvariable=self.statustext, bd=1, relief='sunken', anchor='w')
+        self.status = tk.Label(self.root, textvariable=self.statustext, bd=1, relief='sunken', anchor='w')
         self.status.pack(side='bottom', fill='x')
 
 
-'''
-        self.screenX = self.root.winfo_screenmmwidth()
-        self.screenY = self.root.winfo_screenheight()
-'''
-
 if __name__ == '__main__':
-    root = tk.Tk()
-    MainApplication(root, 'TomSort').pack(side="top", fill="both", expand=True)
-    root.mainloop()
+    app = MainApplication('TomSort')
+    app.mainloop()
 
 '''
 workfolder = Path("C:/Users/tomod/Desktop/FotoSortTest")
